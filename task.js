f22ebeef08ace693a7221f376d44121bee4958f3
fme7 <fatima.mamedova.21@mail.ru>
2024-02-11 21:03:37 -0500

    task down

---
+++ task.js
@@ -0,0 +1,10 @@
+let Ax = Number(prompt("A(x)"));
+let Ay = Number(prompt("A(y)"));
+let Bx = Number(prompt("B(x)"));
+let By = Number(prompt("B(y)"));
+
+let a = Bx - Ax;
+let b = By - Ay;
+let c = a*b;
+
+alert(c)
